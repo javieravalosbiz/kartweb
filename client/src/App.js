de10705713484be4d48881ac16f33d50708034cd
Javier Avalos <javieravalos.biz.gmail.com>
2022-02-28 15:56:32 -0700

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+//css import
+import './App.css';
+//component imports
+import MapList from './components/MapList';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="kart-maps-title">Kart Maps</h1>
+      <MapList />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/MapList.js
@@ -0,0 +1,13 @@
+import './MapList.css'
+
+function MapList(){
+    return (
+    <ul className="map-list">
+        <li>green hill</li>
+        <li>daytona speedway</li>
+        <li>northern district</li>
+    </ul>
+    );
+};
+
+export default MapList;
